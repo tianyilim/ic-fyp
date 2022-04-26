870dc3ad4db2b12b7fced6d76c72ebbcaf62bec3
tianyilim <0.tianyi.lim@gmail.com>
2022-04-26 13:52:38 +0100

    in progress

---
+++ src/multirobot_control/launch/individual_bot.launch.py
@@ -0,0 +1,157 @@
+'''
+Launch file for an individual robot, complete with its own namespace etc.
+'''
+import os
+from tracemalloc import start
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDescription, \
+                            SetEnvironmentVariable, ExecuteProcess
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PythonExpression, Command
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    bringup_dir = get_package_share_directory('multirobot_control')
+    nav2_bringup_dir = get_package_share_directory('nav2_bringup')
+    robot_model_dir = get_package_share_directory('neo_simulation2')
+    warehouse_dir = get_package_share_directory('aws_robomaker_small_warehouse_world')
+    
+    # Arguments for namespacing
+    namespace = LaunchConfiguration('namespace')
+    use_namespace = LaunchConfiguration('use_namespace')
+    x_pose = LaunchConfiguration('x_pose')
+    y_pose = LaunchConfiguration('y_pose')
+    z_pose = LaunchConfiguration('z_pose')
+    yaw_pose = LaunchConfiguration('yaw_pose')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    slam = LaunchConfiguration('slam')
+    map_yaml_file = LaunchConfiguration('map')
+    params_file = LaunchConfiguration('params_file')
+    urdf = LaunchConfiguration('urdf')
+
+    # Map fully qualified names to relative ones so the node's namespace can be prepended.
+    # In case of the transforms (tf), currently, there doesn't seem to be a better alternative
+    remappings_tf = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
+    remap_cmd_vel = [('/cmd_vel', 'cmd_vel')]
+
+    declare_namespace_cmd = DeclareLaunchArgument(
+        'namespace',
+        default_value='',
+        description='Top-level namespace')
+
+    declare_use_namespace_cmd = DeclareLaunchArgument(
+        'use_namespace',
+        default_value='false',
+        description='Whether to apply a namespace to the navigation stack')
+
+    declare_x_pose_cmd = DeclareLaunchArgument(
+        'x_pose',
+        default_value='0.0',
+        description='Initial x-pose of robot')
+
+    declare_y_pose_cmd = DeclareLaunchArgument(
+        'y_pose',
+        default_value='0.0',
+        description='Initial y-pose of robot')
+
+    declare_z_pose_cmd = DeclareLaunchArgument(
+        'z_pose',
+        default_value='0.01',
+        description='Initial z-pose of robot')
+
+    declare_yaw_pose_cmd = DeclareLaunchArgument(
+        'yaw_pose',
+        default_value='3.14',
+        description="Initial yaw of robot"
+    )
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Use simulation (Gazebo) clock if true')
+
+    declare_slam_cmd = DeclareLaunchArgument(
+        'slam',
+        default_value='False',
+        description='Whether run a SLAM')
+
+    declare_map_yaml_cmd = DeclareLaunchArgument(
+        'map', default_value=os.path.join(
+            warehouse_dir, 'maps', '005', 'map.yaml' ),
+        description='Full path to map yaml file to load')
+
+    declare_params_file_cmd = DeclareLaunchArgument(
+        'params_file',
+        default_value=os.path.join(bringup_dir, 'params', 'nav2_params.yaml'),
+        description='Full path to the ROS2 parameters file to use for all launched nodes')
+
+    declare_urdf_cmd = DeclareLaunchArgument(
+        'urdf',
+        default_value=os.path.join(robot_model_dir, 'robots', 'mp_400', 'mp_400.urdf'),
+        description='Full path to robot URDF to load'
+    )
+
+    # os.environ["GAZEBO_MODEL_PATH"] = ''
+
+    # Start node commands
+    spawn_entity_cmd = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[ '-entity', namespace,
+                    '-robot_namespace', namespace,
+                   '-file', urdf,
+                    '-x', x_pose,
+                    '-y', y_pose,
+                    '-z', z_pose,
+                    '-Y', yaw_pose],
+        output='screen',
+        remappings=remappings_tf
+    )
+
+    start_robot_state_publisher_cmd = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        namespace=namespace,
+        output='screen',
+        remappings=remappings_tf,
+        parameters=[{'use_sim_time': use_sim_time},
+            {'robot_description': Command(['xacro ', urdf])}
+        ]
+    )
+
+    rviz_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(nav2_bringup_dir, 'launch', 'rviz_launch.py')),
+        launch_arguments={'namespace': namespace,
+                          'use_namespace': 'True',
+                          'use_sim_time': use_sim_time,
+                          'rviz_config': os.path.join(
+                                nav2_bringup_dir, 'rviz', 'nav2_default_view.rviz')   
+                          }.items()
+    )
+
+
+    ld = LaunchDescription()
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_use_namespace_cmd)
+    ld.add_action(declare_x_pose_cmd)
+    ld.add_action(declare_y_pose_cmd)
+    ld.add_action(declare_z_pose_cmd)
+    ld.add_action(declare_yaw_pose_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_slam_cmd)
+    ld.add_action(declare_map_yaml_cmd)
+    ld.add_action(declare_params_file_cmd)
+    ld.add_action(declare_urdf_cmd)
+    
+    ld.add_action(spawn_entity_cmd)
+    ld.add_action(start_robot_state_publisher_cmd)
+    # ld.add_action(rviz_cmd)
+
+    return ld
--- src/multirobot_control/launch/spawn_bots.launch.py
+++ src/multirobot_control/launch/spawn_bots.launch.py
@@ -25,7 +25,7 @@ from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDesc
                             SetEnvironmentVariable, ExecuteProcess
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.substitutions import LaunchConfiguration, PythonExpression, TextSubstitution
 from launch_ros.actions import PushRosNamespace
 
 
@@ -35,56 +35,26 @@ def generate_launch_description():
     nav2_bringup_dir = get_package_share_directory('nav2_bringup')
     robot_model_dir = get_package_share_directory('neo_simulation2')
     warehouse_dir = get_package_share_directory('aws_robomaker_small_warehouse_world')
-    
-    launch_dir = os.path.join(bringup_dir, 'launch')
 
     robots = [
-        {'name': 'robot1', 'x_pose': 0.0, 'y_pose':  0.5, 'z_pose': 0.01},
-        # {'name': 'robot2', 'x_pose': 0.0, 'y_pose': -0.5, 'z_pose': 0.01}
+        {'name': 'robot1', 'x_pose': 0.0, 'y_pose':  0.5, 'z_pose': 0.01, 'yaw_pose': 0.0},
+        # {'name': 'robot2', 'x_pose': 0.0, 'y_pose': -0.5, 'z_pose': 0.01, 'yaw_pose': 0.0}
     ]
 
     # Create the launch configuration variables
-    namespace = LaunchConfiguration('namespace')
-    use_namespace = LaunchConfiguration('use_namespace')
-    slam = LaunchConfiguration('slam')
     map_yaml_file = LaunchConfiguration('map')
-    use_sim_time = LaunchConfiguration('use_sim_time')
     params_file = LaunchConfiguration('params_file')
+    use_sim_time = LaunchConfiguration('use_sim_time')
     autostart = LaunchConfiguration('autostart')
     world = LaunchConfiguration('world')
 
     stdout_linebuf_envvar = SetEnvironmentVariable(
         'RCUTILS_LOGGING_BUFFERED_STREAM', '1')
 
-    declare_namespace_cmd = DeclareLaunchArgument(
-        'namespace',
-        default_value='',
-        description='Top-level namespace')
-
-    declare_use_namespace_cmd = DeclareLaunchArgument(
-        'use_namespace',
-        default_value='false',
-        description='Whether to apply a namespace to the navigation stack')
-
-    declare_slam_cmd = DeclareLaunchArgument(
-        'slam',
-        default_value='False',
-        description='Whether run a SLAM')
-
-    declare_map_yaml_cmd = DeclareLaunchArgument(
-        'map', default_value=os.path.join(
-            warehouse_dir, 'maps', '005', 'map.yaml' ),
-        description='Full path to map yaml file to load')
-
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
         default_value='false',
         description='Use simulation (Gazebo) clock if true')
-
-    declare_params_file_cmd = DeclareLaunchArgument(
-        'params_file',
-        default_value=os.path.join(bringup_dir, 'params', 'nav2_params.yaml'),
-        description='Full path to the ROS2 parameters file to use for all launched nodes')
 
     declare_autostart_cmd = DeclareLaunchArgument(
         'autostart', default_value='true',
@@ -97,6 +67,16 @@ def generate_launch_description():
         description="Full path to world file"
     )
 
+    declare_map_yaml_cmd = DeclareLaunchArgument(
+        'map', default_value=os.path.join(
+            warehouse_dir, 'maps', '005', 'map.yaml' ),
+        description='Full path to map yaml file to load')
+
+    declare_params_file_cmd = DeclareLaunchArgument(
+        'params_file',
+        default_value=os.path.join(bringup_dir, 'params', 'nav2_params.yaml'),
+        description='Full path to the ROS2 parameters file to use for all launched nodes')
+
     # Launch commands
 
     start_gazebo_server_cmd = ExecuteProcess(
@@ -107,40 +87,26 @@ def generate_launch_description():
         cmd=['gzclient'],
         cwd=[warehouse_dir], output='screen')
 
-    
-    bringup_cmd_group = GroupAction([
-        PushRosNamespace(
-            condition=IfCondition(use_namespace),
-            namespace=namespace),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(launch_dir, 'slam_launch.py')),
-            condition=IfCondition(slam),
-            launch_arguments={'namespace': namespace,
-                              'use_sim_time': use_sim_time,
-                              'autostart': autostart,
-                              'params_file': params_file}.items()),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(launch_dir,
-                                                       'localization_launch.py')),
-            condition=IfCondition(PythonExpression(['not ', slam])),
-            launch_arguments={'namespace': namespace,
-                              'map': map_yaml_file,
-                              'use_sim_time': use_sim_time,
-                              'autostart': autostart,
-                              'params_file': params_file,
-                              'use_lifecycle_mgr': 'false'}.items()),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(launch_dir, 'navigation_launch.py')),
-            launch_arguments={'namespace': namespace,
-                              'use_sim_time': use_sim_time,
-                              'autostart': autostart,
-                              'params_file': params_file,
-                              'use_lifecycle_mgr': 'false',
-                              'map_subscribe_transient_local': 'true'}.items()),
-    ])
+    spawn_robot_cmds = []
+    for robot in robots:
+        spawn_robot_cmds.append(
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(bringup_dir, 'launch', 'individual_bot.launch.py')
+                ),
+                launch_arguments={
+                    'namespace': robot['name'],
+                    'use_namespace': 'True',
+                    'map': map_yaml_file,
+                    'use_sim_time': 'True',
+                    'params': params_file,
+                    'x_pose': TextSubstitution(text=str(robot['x_pose'])),
+                    'y_pose': TextSubstitution(text=str(robot['y_pose'])),
+                    'z_pose': TextSubstitution(text=str(robot['z_pose'])),
+                    'yaw_pose': TextSubstitution(text=str(robot['yaw_pose'])),
+                }.items()
+            )
+        )
 
     # Create the launch description and populate
     ld = LaunchDescription()
@@ -149,18 +115,16 @@ def generate_launch_description():
     ld.add_action(stdout_linebuf_envvar)
 
     # Declare the launch options
-    ld.add_action(declare_namespace_cmd)
-    ld.add_action(declare_use_namespace_cmd)
-    ld.add_action(declare_slam_cmd)
-    ld.add_action(declare_map_yaml_cmd)
     ld.add_action(declare_use_sim_time_cmd)
-    ld.add_action(declare_params_file_cmd)
     ld.add_action(declare_autostart_cmd)
     ld.add_action(declare_world_cmd)
+    ld.add_action(declare_map_yaml_cmd)
+    ld.add_action(declare_params_file_cmd)
 
     # Add the actions to launch all of the navigation nodes
     ld.add_action(start_gazebo_server_cmd)
     ld.add_action(start_gazebo_client_cmd)
-    # ld.add_action(bringup_cmd_group)
+    for cmd in spawn_robot_cmds:
+        ld.add_action(cmd)
 
     return ld
