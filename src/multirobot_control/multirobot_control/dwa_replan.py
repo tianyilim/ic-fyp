431331233198b4ba37f62da2ba93e45639a5c6e0
tianyilim <0.tianyi.lim@gmail.com>
2022-06-06 11:57:47 +0100

    Migrate abort behaviour to be polled in another timer callback

--- src/multirobot_control/multirobot_control/dwa_replan.py
+++ src/multirobot_control/multirobot_control/dwa_replan.py
@@ -42,6 +42,9 @@ class DWAReplanServer(DWABaseNode):
         self.create_timer(1/2, self._get_rrt_timer_callback)
         self.curr_rrt_client = self.create_client(GetRRTWaypoints, f'{self.get_namespace()}/rrt_star_action_server/get_rrt_waypoints')
 
+        # Check if replan
+        self.create_timer(1/2, self._check_replan_timer_callback)
+
         self._last_replan = Time(seconds=0, nanoseconds=0, clock_type=self.get_clock().clock_type)
         '''The timestamp where replanning was done due to stall detection.'''
 
@@ -49,93 +52,50 @@ class DWAReplanServer(DWABaseNode):
         self._curr_manhattan_dist = None
         self._target_manhattan_dist = None
 
-    # override stall callback
-    def stall_detection_callback(self):
+    def _check_replan_timer_callback(self):
         '''
-        Checks if the robot has moved a certain threshold distance within a certain time. 
+        Periodically checks if a replan is necessary. Critiera:
+        - If the nearest robot is closer than the current goal, and the nearest robot is 
+        in between the current goal and this robot's location.
+        - We roughly determine if the nearest robot is in between by comparing their relative
+        bearings. If the difference in bearing is less than ~70 deg (a tunable parameter) we
+        consider it to be "in between"
+        - If the current robot's RRT manhattan distance is greater than the nearest robot's
+        manhattan distance
+        - If the last replan was more than `replan_duration` ago
+        '''
+        if self.closest_robot_pos is not None:
+            # Check distances
+            dist_to_goal = self.distToGoal(self._x, self._y, self.goal_x, self.goal_y)
+            dist_to_robot = self.distToGoal(self._x, self._y, self.closest_robot_pos[0],
+                self.closest_robot_pos[1])
+
+            if dist_to_robot < dist_to_goal:
+                # Check bearing
+                bearing_to_goal = np.arctan2((self.goal_y-self._y), (self.goal_x-self._x))
+                bearing_to_robot = np.arctan2((self.closest_robot_pos[1]-self._y), (self.closest_robot_pos[0]-self._x))
+                bearing_diff = np.abs(bearing_to_goal - bearing_to_robot)
+
+                if np.degrees(bearing_diff) < 70.0:
+                    # Get the current time
+                    now_time_f = self.get_clock().now().seconds_nanoseconds()
+                    now_time_f = now_time_f[0] + now_time_f[1]/1e9
+                    last_replan_f = self._last_replan.seconds_nanoseconds()
+                    last_replan_f = last_replan_f[0] + last_replan_f[1]/1e9
+                    time_diff = now_time_f - last_replan_f
+
+                    if self._curr_manhattan_dist > self._target_manhattan_dist:
+
+                        if time_diff > self.params['replan_duration']:
+                            self.get_logger().info(f"{self.get_namespace()} aborting current goal.")
+                            self._last_replan = self.get_clock().now()
+                            self.set_planner_state(PlannerStatus.PLANNER_ABORT)
+                        else:
+                            self.get_logger().info(f"Time since last replanning: {time_diff:.2f}. Waiting for {self.params['replan_duration']}.")
         
-        If not, perform recovery action:
-        - If dist to obstacle is too low, propose a vector that moves away from obstacle at large speed
-        - Else, propose a random vector (break out of local minima)
-        '''
-
-        if self._planner_state == PlannerStatus.PLANNER_EXEC:
-            if self._dist_travelled < self.params['stall_dist_thresh']:
-                local_goal_x = self.goal_x
-                local_goal_y = self.goal_y
-                
-                goal_poses = [] # A collection of poses that are made of vectors away from potential obstacles
-
-                for robot_name in self.other_robots.keys():
-                    robot_x, robot_y = self.other_robots[robot_name]
-                    dist_to_robot = self.distToGoal( self._x, self._y, robot_x, robot_y )
-                    self.get_logger().info(f'{self.get_namespace()} dist to {robot_name}: {dist_to_robot:.2f}')
-                    # Calculate a path away from the robots that are too close
-                    if dist_to_robot < (self.params['inter_robot_dist']*self.params['robot_radius']):
-                        # Compare RRT Manhattan distances. 
-                        # If distance is further:
-                        self.get_logger().info(f"Curr Dist: {self._curr_manhattan_dist:.2f} Target Dist: {self._target_manhattan_dist:.2f}")
-
-                        if self._curr_manhattan_dist > self._target_manhattan_dist:
-                            now_time_f = self.get_clock().now().seconds_nanoseconds()
-                            now_time_f = now_time_f[0] + now_time_f[1]/1e9
-                            last_replan_f = self._last_replan.seconds_nanoseconds()
-                            last_replan_f = last_replan_f[0] + last_replan_f[1]/1e9
-                            time_diff = now_time_f - last_replan_f
-                            if time_diff > self.params['replan_duration']:
-                                # Abort current goal (await replanning)
-                                self.get_logger().info(f"{self.get_namespace()} aborting current goal.")
-                                self._last_replan = self.get_clock().now()
-                                self.set_planner_state(PlannerStatus.PLANNER_ABORT)
-                            else:
-                                self.get_logger().info(f"Time since last replanning: {time_diff:.2f}. Waiting for {self.params['replan_duration']}.")
-                    
-                        # Set a timer to clear the `replan` flag (eg. 5-10s) so that replanning only happens once in awhile
-                        # Tag this replan flag to name in case of multiple robots getting tangled together
-
-                        self_pos = np.array((self._x, self._y))
-                        robot_pos = np.array((robot_x, robot_y))
-                        goal_poses.append( get_point_on_connecting_line(robot_pos, self_pos, (self.params['inter_robot_dist']*self.params['robot_radius'])) )
-
-                for obstacle in OBSTACLE_ARRAY:
-                    dist_to_obstacle, closest_point = dist_to_aabb(self._x, self._y, obstacle, get_closest_point=True)
-                    if dist_to_obstacle < 1.5*self.params['robot_radius']:
-                        # Set local goal to be twice the robot radius away from the closest point at a right angle.
-                        
-                        self.get_logger().info(f"Robot too close to obstacle. Planning away from it.")
-
-                        self_pos = np.array((self._x, self._y))
-                        robot_pos = np.array((closest_point[0], closest_point[1]))
-                        goal_poses.append( get_point_on_connecting_line(robot_pos, self_pos, self.params['robot_radius']*1.5) )
-
-                # Overwrite the goal vector by taking the mean of all proposed vectors
-                if len(goal_poses) > 0:
-                    goal_pose = np.mean(np.array(goal_poses), axis=0)
-                    local_goal_x = goal_pose[0]
-                    local_goal_y = goal_pose[1]
-
-                # If we are not too close to an obstacle, move toward the goal on an arc
-                goal_hdg = np.arctan2(local_goal_y-self._y, local_goal_x-self._x)
-                hdg_diff = goal_hdg - self._yaw
-                # Sine and Cosine of the heading diff informs our proposed vector, 
-                # So long we use the same hypotenuse (choose the linear speed limit so 
-                # it's not possible to exceed it)
-                # We cap the linear and angular speed limit so they are not exceeded
-                lin_twist = min(
-                    (self.params['linear_speed_limit'])*np.cos(hdg_diff),
-                     self.params['linear_speed_limit'])
-                ang_twist = min(
-                    (self.params['linear_speed_limit'])*np.sin(hdg_diff),
-                     self.params['linear_speed_limit'])
-
-                # We also discretize the values to one of the steps of the planner.
-                self._linear_twist = self.params['linear_step']*np.round(lin_twist/self.params['linear_step'])
-                self._angular_twist = self.params['angular_step']*np.round(ang_twist/self.params['angular_step'])
-
-                self.get_logger().info(f"Stall detected. Moving towards local goal {local_goal_x:.2f},{local_goal_y:.2f} with vect {self._linear_twist:.2f}, {self._angular_twist:.2f}")
-
-        # Reset distance travelled accumulator
-        self._dist_travelled = 0
+        else:
+            # No nearby robot for replanning to take place
+            pass
 
     def handle_other_robot_state(self, msg):
         '''
@@ -165,7 +125,6 @@ class DWAReplanServer(DWABaseNode):
                     # Remove reference to this variable
                     del self.target_rrt_client
                     self.get_logger().info(f"{past_closest_robot} out of range, deleting RRT query client.")
-
 
     def _get_rrt_timer_callback(self):
         '''Periodically polls the RRT servers to get info about the high-level overview of the plan.'''
