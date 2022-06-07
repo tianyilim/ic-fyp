08a64dfee57cc5934d8a91a98f805f01f2bf0da2
tianyilim <0.tianyi.lim@gmail.com>
2022-06-07 21:17:11 +0100

    Testbench stuff

--- src/multirobot_control/create_test_scenarios.py
+++ src/multirobot_control/create_test_scenarios.py
@@ -25,8 +25,8 @@ scenario_settings = {
     'robot_starting_x': [-5.5],
     'robot_starting_y': [1.15],
     'robot_starting_theta': [0.0],
-    'total_goals': 1,
-    'watchdog_timeout_s': 30,
+    'total_goals': 10,
+    'watchdog_timeout_s': 240,
     'result_folder': "result",
     'params_filepath': "/home/tianyilim/fyp/ic-fyp/src/multirobot_control/params/planner_params.yaml",
     # If random goals are desired, leave this as an empty list
@@ -67,48 +67,29 @@ param_settings = {
     'rrt_debug_plot' : False,
     'waypoint_skip': True,
     'waypoint_replan': False,
-    'local_planner': 'dwa_replan_server',
+    'local_planner': 'dwa_action_server',
 
     # Typically for odom_distribution node,
     'odom_dist_thresh': 2.0,
 }
 
-# Scenario parameters
-num_robots = range(1,7)
-# Set initial robot x,y,theta as well
-total_goals = 10
-timeout = 120
-result_folder = ""
-params_filepath = "/home/tianyilim/fyp/ic-fyp/src/multirobot_control/params/planner_params.yaml"
-
 # Tuning parameters
-local_planner = ['dwa_action_server', 'dwa_multirobot_server','dwa_replan_server']
-simulate_duration = []
-action_duration = []
-linear_speed_limit = []
-angular_speed_limit = []
-linear_step : 0.1
-angular_step : 0.2
-dist_thresh_hi : 0.2
-dist_thresh_lo : 0.05
-orientation_ub_deg : 180.0
-orientation_lb_deg : 20.0
-angular_K : 1.0 # the max proportion of dist_to_goal that angular contributes
-goal_K : 10.0
-obstacle_K: 1.0
-stall_det_period: 1.0
-stall_dist_thresh: 0.1
-replan_duration: 5.0  # How long to wait to replan when an obstacle is reached
-rrt_it_lim : 2000
-rrt_it_min : 200
-rrt_max_extend_length : 1.5
-rrt_connect_circle_dist : 1.5
+# local_planner = ['dwa_action_server', 'dwa_multirobot_server','dwa_replan_server']
 
-LIST_OF_POSSIBLE_SETTINGS = [0.4, 0.5]
+# TODO : Set this to something legit
+# For now changes max speed from 0.4 to 0.7 and runs each value 5 times.
+LIST_OF_POSSIBLE_SETTINGS = [
+    0.6, 0.6, 0.6, 0.6, 0.6,
+    0.6, 0.6, 0.6, 0.6, 0.6,
+    0.6, 0.6, 0.6, 0.6, 0.6,
+    0.6, 0.6, 0.6, 0.6, 0.6,
+    0.6, 0.6, 0.6, 0.6, 0.6,
+]
 
 for i, setting in enumerate(LIST_OF_POSSIBLE_SETTINGS):
-    scenario_filename = f"/home/tianyilim/fyp/ic-fyp/src/multirobot_control/test_scenarios/{i}.yaml"
-    param_filename = f"/home/tianyilim/fyp/ic-fyp/src/multirobot_control/test_params/{i}.yaml"
+    filename = f"linear_speed_limit_{setting}_{i}"
+    scenario_filename = f"/home/tianyilim/fyp/ic-fyp/src/multirobot_control/test_scenarios/{filename}.yaml"
+    param_filename = f"/home/tianyilim/fyp/ic-fyp/src/multirobot_control/test_params/{filename}.yaml"
 
     scenario_settings_copy = copy.deepcopy(scenario_settings)
     param_settings_copy = copy.deepcopy(param_settings)
