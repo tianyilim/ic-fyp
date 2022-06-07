e8cec05d3865bf6fe68971528248c979c371be23
tianyilim <0.tianyi.lim@gmail.com>
2022-06-07 18:02:14 +0100

    pipe output of ROS stuff to log file

--- src/multirobot_control/evaluate_planners.bash
+++ src/multirobot_control/evaluate_planners.bash
@@ -5,6 +5,8 @@
 rm -r /home/tianyilim/fyp/ic-fyp/src/multirobot_control/test_scenarios
 rm -r /home/tianyilim/fyp/ic-fyp/src/multirobot_control/test_params
 
+mkdir -p result/log
+
 # Creates a few variations of test parameters in 
 python3 create_test_scenarios.py
 
@@ -22,18 +24,18 @@ do
     ros2 run multirobot_control goal_creation --ros-args --params-file "/home/tianyilim/fyp/ic-fyp/src/multirobot_control/params/scenario_custom.yaml" &
     GOAL_CREATION_PID=$!
 
-    ros2 launch multirobot_control test_scenario.launch.py rviz:=false &
+    LOGFILE=$(basename $file .yaml)
+    touch "result/log/${LOGFILE}.txt"
+    ros2 launch multirobot_control test_scenario.launch.py rviz:=false > "log/${LOGFILE}.txt" &
 
     wait $GOAL_CREATION_PID # Wait for goal_creation to terminate.
     echo "Goal creation completed. Killing all ROS nodes."
 
     # Find all PIDs of running ROS processes
     pid_array=$(ps aux | grep [^a-zA-Z]ros[^a-zA-Z] | awk '{print $2}')
-    for i in "${pid_array[@]}"
-    do
-        kill $i
-    done
+    for PID in "${pid_array[@]}"; do kill $PID; done
+
 done
 
-rm "/home/tianyilim/fyp/ic-fyp/src/multirobot_control/params/scenario_custom.yaml"
-rm "/home/tianyilim/fyp/ic-fyp/src/multirobot_control/params/params_custom.yaml"
+rm -f "/home/tianyilim/fyp/ic-fyp/src/multirobot_control/params/scenario_custom.yaml"
+rm -f "/home/tianyilim/fyp/ic-fyp/src/multirobot_control/params/params_custom.yaml"
